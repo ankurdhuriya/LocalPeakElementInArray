d7109be2651591b34d38bda90cdbbbfa0c21eec8
haanjiankur <mail.ankurdhuriya@gmail.com>
2020-02-18 16:31:46 +0530

    Add. peak element for 2 element array with left one be greater.

--- peak_element_test.py
+++ peak_element_test.py
@@ -4,6 +4,9 @@ import unittest
 def find_peak(arr):
     if len(arr) == 1:
         return arr[0]
+    if len(arr) > 1:
+        if arr[0] > arr[1]:
+            return arr[0]
     return None
 
 
@@ -14,6 +17,8 @@ class MyTestCase(unittest.TestCase):
     def test_single_element_array_have_that_element_as_peak(self):
         self.assertEqual(2, find_peak([2]))
 
+    def test_two_element_array_with_left_element_as_peak(self):
+        self.assertEqual(2, find_peak([2, 1]))
 
 if __name__ == '__main__':
     unittest.main()
